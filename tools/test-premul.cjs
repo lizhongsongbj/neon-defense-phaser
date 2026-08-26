331ab35a61e71f4c7a29c895d36d70ef7514a92d
Ambition571 <Ambition571@users.noreply.github.com>
2026-08-26 16:50:03 +0800

    Auto-sync 2026-08-26 16:50:03

--- tools/test-premul.cjs
+++ tools/test-premul.cjs
@@ -1,2 +1,2 @@
 ﻿const sharp=require('sharp');
-(async()=>{const src='public/assets/enemies/new/enemy-06-faraday-suppressor.png'; const a=await sharp(src).ensureAlpha().premultiply().rotate(55,{background:{r:0,g:0,b:0,alpha:0}}).resize(420,420,{fit:'inside'}).unpremultiply().png().toFile('tools/test-premul.png'); console.log(a)})()
+(async()=>{const {data,info}=await sharp('public/assets/enemies/new/enemy-06-faraday-suppressor.png').ensureAlpha().raw().toBuffer({resolveWithObject:true});for(let i=0;i<data.length;i+=4){const a=data[i+3]/255;data[i]=Math.round(data[i]*a);data[i+1]=Math.round(data[i+1]*a);data[i+2]=Math.round(data[i+2]*a)}await sharp(data,{raw:info}).rotate(55,{background:{r:0,g:0,b:0,alpha:0}}).resize(420,420,{fit:'inside'}).png().toFile('tools/test-premul.png')})()
