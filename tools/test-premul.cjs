0bc034fcb0a2274d2e8285abf904ff3c30206ff7
Ambition571 <Ambition571@users.noreply.github.com>
2026-08-26 16:49:46 +0800

    Auto-sync 2026-08-26 16:49:46

---
+++ tools/test-premul.cjs
@@ -0,0 +1,2 @@
+﻿const sharp=require('sharp');
+(async()=>{const src='public/assets/enemies/new/enemy-06-faraday-suppressor.png'; const a=await sharp(src).ensureAlpha().premultiply().rotate(55,{background:{r:0,g:0,b:0,alpha:0}}).resize(420,420,{fit:'inside'}).unpremultiply().png().toFile('tools/test-premul.png'); console.log(a)})()
